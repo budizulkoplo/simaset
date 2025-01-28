b722f0192e999caabef0742ebdede5c3f6c334fb
Dzulfikar Budi Andreanto <139447405+budizulkoplo@users.noreply.github.com>
2025-01-28 15:50:03 +0700

    up

--- app/Views/admin/layout/menunew.php
+++ app/Views/admin/layout/menunew.php
@@ -15,8 +15,8 @@ $username=$session->get('nama');
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="<?= base_url('admin/dasbor') ?>" class="brand-link">
-      <img src="<?php echo base_url('assets/upload/image/'.$site['icon']) ?>" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light"><?php echo $site['singkatan'] ?></span>
+      <img src="<?php echo base_url('assets/upload/image/'.$site['icon']) ?>" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8; background-color: white; border-radius: 50%; padding: 5px;">
+      <span class="brand-text font-weight-light"><?php echo $site['namaweb'] ?></span>
     </a>
 
     <!-- Sidebar -->
